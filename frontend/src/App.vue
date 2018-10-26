9f22a5705824816431be2dec8c1f4023fff15112
Felix Wu <mail@flxwu.com>
2018-10-26 23:19:54 +0200

    setup here map

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="mapContainer" ref="mapContainer"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const platform = new H.service.Platform({
+  app_id: process.env.VUE_APP_HEREID,
+  app_code: process.env.VUE_APP_HERECODE
+});
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  mounted: function() {
+    const defaultLayers = platform.createDefaultLayers();
+    // Instantiate (and display) a map object:
+    const map = new H.Map(
+      this.$refs.mapContainer,
+      defaultLayers.normal.map,
+      {
+        zoom: 10,
+        center: { lat: 52.5, lng: 13.4 }
+      }
+    );
+    const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+    // Create the default UI components
+    const ui = H.ui.UI.createDefault(map, defaultLayers);
   }
-}
+};
 </script>
 
 <style>
+body {
+  margin: 0
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
 }
+
+#mapContainer {
+  width: 100%;
+  height: 100%;
+}
+
 </style>
