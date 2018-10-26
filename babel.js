9c8798ab939241e066c8b2815c1648393a467021
Felix Wu <mail@flxwu.com>
2018-10-26 19:31:15 +0200

    add babel compiler

--- babel.js
+++ babel.js
@@ -1,7 +1,8 @@
 // Transpile all code following this line with babel and use 'env' (aka ES6) preset.
-require('babel-register')({
-	presets: [ 'env' ]
-})
+require('@babel/register')({
+  presets: [ '@babel/env' ],
+  plugins: ['@babel/plugin-transform-async-to-generator']
+});
 
 // Import the rest of our application.
-module.exports = require('./app.js')
+module.exports = require('./app.js');
