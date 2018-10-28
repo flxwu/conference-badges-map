7c0beff1d2bebe40a0efefa6577e23575074779e
Felix Wu <mail@flxwu.com>
2018-10-28 15:22:43 +0100

    add vue-select

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,56 @@
 <template>
   <div id="app">
-    <Controls />
+    <ScanBadge v-if="isBadgeScanShown" v-on:onScanFinish="onScanFinish"/>
+    <FilterBadgeData v-else v-bind:textStrings="textArray"/>
     <Map />
   </div>
 </template>
 
 <script>
 import Map from './components/Map.vue';
-import Controls from './components/Controls.vue';
+import ScanBadge from './components/ScanBadge.vue';
+import FilterBadgeData from './components/FilterBadgeData.vue';
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      textArray: [],
+      isBadgeScanShown: true
+    };
+  },
   components: {
     Map,
-    Controls
+    ScanBadge,
+    FilterBadgeData
+  },
+  methods: {
+    onFilterCloseFinish: function() {
+      this.isBadgeScanShown = true;
+    },
+    onScanFinish: function(textArray) {
+      this.textArray = Array.from(
+        new Set(
+          textArray
+            .sort((t1, t2) => t2.confidence - t1.confidence)
+            .map(obj => obj.text)
+        )
+      );
+      this.isBadgeScanShown = false;
+    }
   }
 };
 </script>
 
-<style>
-body {
-  margin: 0;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  width: 100vw;
-  height: 100vh;
-}
+<style lang="sass">
+body
+  margin: 0
+
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+  width: 100vw
+  height: 100vh
 </style>
